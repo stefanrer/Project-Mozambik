9304e7f265510d0d9f52122ddd2a93e32032a8a6
stefanrer <robstef85@gmail.com>
2016-10-20 00:06:19 +0300

    Add files via upload

---
+++ primer.js
@@ -0,0 +1,25 @@
+﻿function show_test() {
+    var c1_elem = document.getElementById("c1");
+    var c2_elem = document.getElementById("c2");
+    var c3_elem = document.getElementById("c3");
+    c1_elem.style.display = 'none';
+    c2_elem.style.display = 'block';
+    c3_elem.style.display = 'none';
+}
+function show_textpic() {
+    var c1_elem = document.getElementById("c1");
+    var c2_elem = document.getElementById("c2");
+    var c3_elem = document.getElementById("c3");
+    c1_elem.style.display = 'block';
+    c2_elem.style.display = 'none';
+    c3_elem.style.display = 'none';
+}
+function show_map() {
+    var c1_elem = document.getElementById("c1");
+    var c2_elem = document.getElementById("c2");
+    var c3_elem = document.getElementById("c3");
+    c1_elem.style.display = 'none';
+    c2_elem.style.display = 'none';
+    c3_elem.style.display = 'block';
+}
+Вставляеш все между тэгами script
